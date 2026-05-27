25045842e3cc3545e2e4e6b9dd01384593b3fea6
riddler <gimperro.business.contact@gmail.com>
2026-05-27 02:33:56 +0200

    demo 4.43

--- src/js/script.js
+++ src/js/script.js
@@ -310,7 +310,7 @@ function robinGada(text) {
     if (!speechSynth.speaking && enteredText.trim().length) {
         // error.textContent = "";
         const newUtter = new SpeechSynthesisUtterance(enteredText);
-        newUtter.rate = 1.25;
+        newUtter.rate = 1.20;
         newUtter.voice = voices[0];
         speechSynth.speak(newUtter);
         // convertBtn.textContent = "Sound is Playing..."
